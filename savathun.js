7cf0bcdbd0086baf3cd88e80446071fd38d4ac9a
Alasindera <68355000+Alasindera@users.noreply.github.com>
2020-07-15 18:19:45 -0400

    Update savathun.js

--- savathun.js
+++ savathun.js
@@ -7,13 +7,36 @@ var savathunSongLines = [
 "\`[static]\`"
 ];
 
+// If we wanna grow this, commands should exist in an enum or something but this is fine for now because we just goofin'
+var commandSendSongLine = "!song"
+var commandGetRaidTime = "!raidtime"
+var setRaidTime = "!setraid"
+
+// Hard-coding because this is a proof of concept. Obviously it'll require extra logic to actually work.
+var nextRaidTime = "**Thursday, Smarch 35th at 13 PM EDT**"
+
 client.on('ready', () => {
 	console.log ('Logged in as ${client.user.tag}!');
 });
 
 client.on('message', msg => {
-	if (msg.content.includes('test')) {
+	if (msg.content.includes(sendSongLine)) {
 		var randomLine = savathunSongLines[Math.floor(Math.random() * savathunSongLines.length)];
 		msg.channel.send(randomLine); 
+	}
+});
+
+// There are a fuckton of features we could add for raid scheduling. This is just a basic thing to show the idea.
+client.on('message', msg => {
+	if (msg.content.includes(getRaidTime)) {
+		// idk if this is actually how string escapes work in javascript because I never write shit in js, but google says it is
+		msg.channel.send(`The next raid is on ${nextRaidTime}`); 
+	}
+});
+
+client.on('message', msg => {
+	if (msg.content.includes(setRaidTime)) {
+		nextRaidTime = setRaidTime.substr(9);
+		msg.channel.send(`Raid time scheduled! It will take place on ${nextRaidTime}`); 
 	}
 });
